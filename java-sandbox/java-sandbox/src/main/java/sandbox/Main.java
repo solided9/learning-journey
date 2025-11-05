bef83f7724133dbbcf5d37578c49b8b6570f7e00
Eduards Melnicenko <solided9@gmail.com>
2025-11-05 17:56:59 +0200

    Add Main.java

--- java-sandbox/java-sandbox/src/main/java/sandbox/Main.java
+++ java-sandbox/java-sandbox/src/main/java/sandbox/Main.java
@@ -2,6 +2,7 @@ package sandbox;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        System.out.println("Hello from IntelliJ!");
+
     }
 }
